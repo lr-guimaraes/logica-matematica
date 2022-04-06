516ab82f3483d75f5d1d688c58ceae6754ad24fd
lr-guimaraes <leandrorg307@gmail.com>
2022-04-06 00:20:33 -0300

    update

--- tabelavddV2.cpp
+++ tabelavddV2.cpp
@@ -4,51 +4,65 @@
 
 class Operation{
     private:
-        char *preposition[5] = {"n","c","d","i","b"};
-        //struct
+        char *preposition[5] = {"n","c","d","i","b"}; //straighten
+        int result[10][0]; //straighten
+  
     public:
+        void pushVar(char [], int);
+        void pushT(int);
         void operation();
         void conjunction();
         void disjunction();
         void implication();
         void binomial();
         void true_table();       
-}
+};
 
 int main(){
+    Operation op;
     int i = 0, number;
-    char *str[10];
-    std::cout <<"Type of the number variables between 1 and 3";
+    char str[10]; //straighten
+
+    std::cout <<"Type of the number variables: ";
     std::cin >>number;
     do{
         std::cout <<"Type it variable string: ";
         std::cin >>str[i];
         i++;
     }while(i < number);
+    op.pushVar(str, number);
+    op.pushT(number);
 
-    pushVar(str [],number);
-    pushT(number);
-    Operation::true_table();
+    op.true_table();
     
 
 }
-void Operation::pushVar(char str [], int number){
+
+void Operation::pushVar(char str[], int number){
     char *variable = new char[number];
 
-    for(i = 0; i < number ;i++){
+    for(int i = 0; i < number; i++){
         variable[i] = str[i];
-        cout << variable[i] <<";"; //test
+        std::cout << variable[i] <<";"; //test
     }
-    cout <<end1;
+    std::cout <<"\n";
 }
-void Operation::pushT(int coluns){
-    int line = 2**coluns;
-    int *cont = new int[line][coluns];
 
-    for(i = 0; i <line; i++){
-        for(j = 0, j< coluns, j++){
-            cont[i][j] = ;
+void Operation::pushT(int number){
+    int line = 2^number;
+    int **cont = new int[line][number]; //straighten
+    int k = 0;
+
+    for(int i = 0; i <line; i++){
+        for(int j = 0; j<  number; j++){
+            if(k <= (2^j)/2){
+                cont[i][j] = F;
+            }else if((k > (2^j)/2)){
+                cont[i][j] = T;
+            }
+            k++;
         }
+        k = 0;
     }
 }
 
@@ -57,21 +71,21 @@ void Operation::operation(){
     Operation op;     
     op.conjunction();
     op.disjunction();
-    op.mplication();
     op.binomial();
 }
 
 void Operation::conjunction(){
-    for(i = 0; i < 4 ; i++){
-        if((a[i] && b[i]) == T){
+    for(int i = 0; i < sizeof(Operation::cont); i++){
+        if((Operation::cont[i][j] && (Operation::cont[i][j])) == T){
             Operation::result[i][0] = T;
         }else{
             Operation::result[i][0] =F;
         }
     }
 }
+/*
 void Operation::disjunction(){
-    for(i = 0; i < 4 ; i++){
+    for(int i = 0; i < sizeof(Operation::cont) ; i++){
         if((a[i] || b[i]) == T){
             Operation::result[i][1] = T;
         }else{
@@ -80,7 +94,7 @@ void Operation::disjunction(){
     }
 }
 void Operation::implication(){
-    for(i = 0; i < 4 ; i++){
+    for(i = 0; i < sizeof(Operation::cont) ; i++){
         if((b[i]== T || (a[i] || b[i] == F)){
             Operation::result[i][2]= T;
         }else{
@@ -89,7 +103,7 @@ void Operation::implication(){
     }
 }
 void Operation::bicondicional(){
-    for(i = 0; i < 4 ; i++){
+    for(i = 0; i < sizeof(Operation::cont) ; i++){
         if(((a[i] && b[i]) == T || (a[i] && b[i] == F))){
             Operation::result[i][3] = T;
         }else{
@@ -122,3 +136,4 @@ void Operation::true_table(){
     printf(""<<op.b[0]);
 
 }
+*/
